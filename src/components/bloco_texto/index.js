e2a722051c1b978ba767fc6928f5fcb3199b1eec
Cassio Pereira <cassiogomes00@gmail.com>
2021-11-24 18:33:15 -0300

    Alterar estilos da tela ambientes

--- src/components/bloco_texto/index.js
+++ src/components/bloco_texto/index.js
@@ -1,30 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import { useSelector } from 'react-redux';
-import './bloco_texto.css'
+import { Link } from 'react-router-dom';
+import './bloco_texto.css';
 
-function Bloco_texto({key, predio, sala}) {
-    return (
-        <div className="col-md-3 col-sm-12 cartao text-center">
-            <div className="texto-body">
-                <h5>Sala: {sala}</h5>
-                <p className="texto-text text-justify">Prédio: {predio}</p>
+function Bloco_texto({ key, predio, sala }) {
+  return (
+    <div className="cartao">
+      <h5>Sala: {sala}</h5>
+      <p>Prédio: {predio}</p>
 
-                <div className="row rodape-texto d-flex align-items-center">
-                    <div className="col-6">
-                        {
-                            useSelector(state => state.usuarioLogado) > 0 ?
-                                <Link to="agendamento" className="btn btn-sm btn-detalhes">Fazer agendamento</Link>
-                                :
-                                <Link to="login" className="btn btn-sm btn-detalhes">Login</Link>
-                        }
-                    </div>
-
-                </div>
-            </div>
-        </div>
-
-    )
+      {useSelector((state) => state.usuarioLogado) > 0 ? (
+        <Link to="agendamento" className="btn btn-detalhes">
+          Fazer agendamento
+        </Link>
+      ) : (
+        <Link to="login" className="btn btn-detalhes">
+          Login
+        </Link>
+      )}
+    </div>
+  );
 }
 
 export default Bloco_texto;
--- src/view/ambientes/index.js
+++ src/view/ambientes/index.js
@@ -2,37 +2,42 @@ import React, { useEffect, useState } from 'react';
 import Bloco_texto from '../../components/bloco_texto';
 import NavBar from '../../components/navbar';
 import firebase from '../../config/firebase';
+import './ambientes.css';
 
 function Ambientes() {
-    const [ambientes, setAmbientes] = useState([]);
+  const [ambientes, setAmbientes] = useState([]);
 
-    var listaAmbientes = [];
+  var listaAmbientes = [];
 
-    useEffect(() => {
-        firebase.firestore().collection('predios').get().then(async (resultado) => {
-            await resultado.docs.forEach(doc => {
-                listaAmbientes.push({
-                    id: doc.id,
-                    ...doc.data()
-                })
-            })
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('predios')
+      .get()
+      .then(async (resultado) => {
+        await resultado.docs.forEach((doc) => {
+          listaAmbientes.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+        });
 
-            setAmbientes(listaAmbientes);
-        })
-    });
-    return (
-        <body>
-            <NavBar />
-            <div className="col-12 p-3 my-3 bg-dark text-white">
-                <div className="row">
-                    {
-                        ambientes.map(item=><Bloco_texto key={item.id} predio={item.predio} sala={item.sala} />)
-                    }
-                </div>
-            </div>
-
-        </body>
-    );
+        setAmbientes(listaAmbientes);
+      });
+  });
+  return (
+    <body>
+      <NavBar />
+      <div className="ambientes-content bg-dark text-white">
+        <h2>Lista de Ambientes</h2>
+        <div className="lista">
+          {ambientes.map((item) => (
+            <Bloco_texto key={item.id} predio={item.predio} sala={item.sala} />
+          ))}
+        </div>
+      </div>
+    </body>
+  );
 }
 
 export default Ambientes;
