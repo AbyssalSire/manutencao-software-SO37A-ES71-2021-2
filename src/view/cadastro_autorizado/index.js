5b84b3b3b09626c77ed3b409c5cc857a13e292ce
Lucas Malheiros <40677401+AbyssalSire@users.noreply.github.com>
2021-11-23 22:23:02 -0300

    Revert "Implementação da máscara para telefone"

--- src/view/cadastro_autorizado/index.js
+++ src/view/cadastro_autorizado/index.js
@@ -25,28 +25,9 @@ function Cadastro_autorizado() {
   }
 
   function validarTelefoneInput(telefone) {
-    const regex = /^[(]([0-9]{2})[)]([0-9]{9})$/;
+    const regex = /^[\+]?([0-9]{2,3})?[(]?([0-9]{2})[)]?[ ]?([0-9]{4,5})([0-9]{4})$/;
     if (!regex.test(telefone)) {
-      throw new Error("Telefone inválido.")
-    }
-  }
-
-  const mascaraTelefoneRegexpPreenchimento = /^[(]?[0-9]{0,2}[)]?[0-9]{0,9}$/;
-  function aplicarMascaraTelefone(novoTelefone) {
-
-    // 1- tentar colocar espaço entre ")" e o proximo numero (lembrar de trocar no regex do validarEmailInput)
-    // 2- se o primeiro caractere não for número ou "(" a validação quebra 
-
-    if (novoTelefone.indexOf("(") != 0) {
-      novoTelefone = "(" + novoTelefone
-    } 
-
-    if (novoTelefone.length > 3 && novoTelefone.indexOf(")") != 3) {
-      novoTelefone = novoTelefone.slice(0,3) + ")" + novoTelefone.slice(3)
-    }
-
-    if (mascaraTelefoneRegexpPreenchimento.test(novoTelefone)) {
-      setTelefone(novoTelefone);
+      throw new Error("Telefone inválido")
     }
   }
 
@@ -168,13 +149,11 @@ function Cadastro_autorizado() {
             <label for="enunciado">Telefone:</label>
             <br />
             <input
-              onChange={(e) => aplicarMascaraTelefone(e.target.value)}
+              onChange={(e) => setTelefone(e.target.value)}
               type="text"
-              value={telefone}
               className="form-control mx-auto"
               id="pessoa-fone"
               name="pessoa-fone"
-              placeholder="(11) 912345678"
             />
             <br />
           </div>
@@ -187,7 +166,6 @@ function Cadastro_autorizado() {
               className="form-control mx-auto"
               id="pessoa-email"
               name="pessoa-email"
-              placeholder="email@provedor.com"
             />
             <br />
           </div>
