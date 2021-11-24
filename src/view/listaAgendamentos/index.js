ffeed9a3849e8450e2addce68ae2d9d896dc3f4e
farias <fariasjunior96@gmail.com>
2021-11-23 23:55:05 -0300

    agrupamento de status de agendamento

--- src/view/listaAgendamentos/index.js
+++ src/view/listaAgendamentos/index.js
@@ -3,29 +3,49 @@ import ListaAgendamentos from '../../components/listaAgendamentos';
 import NavBar from '../../components/navbar';
 import firebase from '../../config/firebase';
 
+const getGrupoAgendamentoVazio = () => new Object({
+  'cartao-expirado': [],
+  'cartao-reservado': [],
+  'cartao-emandamento': []
+})
+
 function ListaAgendamento() {
-  const [agendamento, setAgendamento] = useState([]);
+  const [agendamento, setAgendamento] = useState(getGrupoAgendamentoVazio());
 
   var listaDeAgendamento = [];
 
-  function ordernarAgendamentos(agendamentos) {
-    console.log(agendamentos);
+  const dataEHora = (data, hora) => new Date(data + 'T' + hora);
 
-    const dataEHora = (data, hora) => new Date(data + 'T' + hora);
+  function statusAgendamento(agendamento) {
+    const agora = new Date();
+    const inicio = dataEHora(agendamento.dataAgendamento, agendamento.horaAgendamentoInicio);
+    const fim = dataEHora(agendamento.dataAgendamento, agendamento.horaAgendamentoFim);
+
+    if (agora > inicio && agora < fim) {
+      return 'cartao-emandamento';
+    }
+
+    if (agora < inicio) {
+      return 'cartao-reservado';
+    }
+
+    return 'cartao-expirado';
+  }
+
+  function ordernarAgendamentos(agendamentos) {
+
     const mapaDatas = {};
     for (const agendamento of agendamentos) {
       const data = agendamento.dataAgendamento;
       if (!mapaDatas[data]) mapaDatas[data] = [];
       mapaDatas[data].push(agendamento);
     }
-    console.log(mapaDatas);
     
     const mapaDatasOrdenado = {};
     for (const data in mapaDatas) {
       mapaDatasOrdenado[data] = mapaDatas[data].sort((a, b) => 
         dataEHora(data, a.horaAgendamentoInicio) > dataEHora(data, b.horaAgendamentoInicio))
     }
-    console.log(mapaDatas);
 
     const agendamentosOrdenados = []
     for (const data in mapaDatasOrdenado) {
@@ -33,10 +53,20 @@ function ListaAgendamento() {
         agendamentosOrdenados.push(agendamento);
       }
     }
-    console.log(agendamentosOrdenados);
 
     return agendamentosOrdenados.sort((a, b) =>
       new Date(a.dataAgendamento) > new Date(b.dataAgendamento));
+  }
+
+  function agruparAgendamentos(agendamentos) {
+    const grupos = getGrupoAgendamentoVazio();
+
+    for (const agendamento of agendamentos) {
+      const status = statusAgendamento(agendamento);
+      grupos[status].push(agendamento);
+    }
+
+    return grupos;
   }
 
   useEffect(() => {
@@ -53,7 +83,9 @@ function ListaAgendamento() {
         });
 
         setAgendamento(
-          ordernarAgendamentos(listaDeAgendamento)
+          agruparAgendamentos(
+            ordernarAgendamentos(listaDeAgendamento)
+          )
         );
       });
   }, []);
@@ -62,9 +94,41 @@ function ListaAgendamento() {
     <body>
       <NavBar />
       <div className="col-12 p-3 my-3 bg-dark text-white">
-        <div className="row">
-          {agendamento.map((item) => (
+      <h3>Em andamento</h3>
+      <div className="row">
+          {agendamento['cartao-emandamento'].map((item) => (
             <ListaAgendamentos
+              status={statusAgendamento(item)}
+              predio={item.predio}
+              sala={item.sala}
+              dataAgendamento={item.dataAgendamento}
+              horaAgendamentoInicio={item.horaAgendamentoInicio}
+              horaAgendamentoFim={item.horaAgendamentoFim}
+              pessoaNome={item.pessoaNome}
+              responsavelPeloAgendamento={item.responsavelPeloAgendamento}
+            />
+          ))}
+        </div>
+        <h3>Reservados</h3>
+        <div className="row">
+          {agendamento['cartao-reservado'].map((item) => (
+            <ListaAgendamentos
+              status={statusAgendamento(item)}
+              predio={item.predio}
+              sala={item.sala}
+              dataAgendamento={item.dataAgendamento}
+              horaAgendamentoInicio={item.horaAgendamentoInicio}
+              horaAgendamentoFim={item.horaAgendamentoFim}
+              pessoaNome={item.pessoaNome}
+              responsavelPeloAgendamento={item.responsavelPeloAgendamento}
+            />
+          ))}
+        </div>
+        <h3>Expirados</h3>
+        <div className="row">
+          {agendamento['cartao-expirado'].map((item) => (
+            <ListaAgendamentos
+              status={statusAgendamento(item)}
               predio={item.predio}
               sala={item.sala}
               dataAgendamento={item.dataAgendamento}
